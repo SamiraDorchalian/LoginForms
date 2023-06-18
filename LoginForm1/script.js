b2b3355d57b7beb9572dc73e609725e39cf953df
samira dorchalian <samiradorchalain1998@gmail.com>
2023-06-18 17:18:23 +0430

    Practice Login Form-1

---
+++ LoginForm1/script.js
@@ -0,0 +1,40 @@
+$('.button').click(function(){
+    var $btn = $(this),
+        $step = $btn.parents('.modal-body'),
+        stepIndex = $step.index(),
+        $pag = $('modal-header span').eq(stepIndex);
+    if (stepIndex === 0 || stepIndex === 1){step1($step , $pag)}
+    else{step3($step , $pag) ;}
+});
+function step1($step , $pag){
+    console.log('step1');
+    $step.addClass('animate-out');
+    setTimeout(function(){
+        $step.removeClass('animate-out is-showing')
+            .next().addClass('animate-in');
+        $pag.removeClass('is-active')
+            .next().addClass('is-active');
+    }, 600);
+    setTimeout(function(){
+        $step.next().removeClass('animate-in')
+            .addClass('is-showing');
+    }, 1200);
+}
+
+function step3($step , $pag){
+    console.log('step3');
+    $step.parents('.modal-wrap').addClass('animate-up');
+    setTimeout(function(){
+        $('.rerun-button').css('display', 'inline-block');
+    }, 300);
+}
+$('.rerun-button').click(function(){
+    $('.modal-wrap').removeClass('animate-up')
+        .find('.modal-body')
+        .first().addClass('is-showing')
+        .siblings().removeClass('is-showing');
+    
+    $('.modal-header span').first().addClass('is-active')
+        .siblings().removeClass('is-active');
+    $(this).hide();
+});
