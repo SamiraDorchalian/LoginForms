67dc8adf086e0088937c22290e2d4ddae22188cf
samira dorchalian <samiradorchalain1998@gmail.com>
2023-12-30 13:49:31 +0330

    LoginForm-5

--- LoginForm5/script.js
+++ LoginForm5/script.js
@@ -8,43 +8,46 @@ const siginBtn = document.querySelector(".signin-button");
 siginBtn.addEventListener("click", signIn);
 
 function signIn(event) {
-    event.preventDefault();
-    usernameMsg.innerText = "";
-    passwordMsg.innerText = "";
-    const usernameValue = usernameInput.value;
-    const passwordValue = passwordInput.value;
-    let ifSendData = true;
+  event.preventDefault();
+  usernameMsg.innerText = "";
+  passwordMsg.innerText = "";
+  const usernameValue = usernameInput.value;
+  const passwordValue = passwordInput.value;
+  let ifSendData = true;
 
-    if (usernameValue.length === 0 || usernameValue.indexOf("@") === -1 || usernameValue.indexOf(".") === -1) {
-        usernameMsg.innerText = "Please enter a valid Email";
-        ifSendData = false;
-    }
+  if (
+    usernameValue.length === 0 ||
+    usernameValue.indexOf("@") === -1 ||
+    usernameValue.indexOf(".") === -1
+  ) {
+    usernameMsg.innerText = "Please enter a valid Email";
+    ifSendData = false;
+  }
 
-    if (passwordValue.length === 0) {
-        passwordMsg.innerText = "Please enter your password";
-        ifSendData = false;
-    } else if (passwordValue.length <= 4) {
-        passwordMsg.innerText = "Your password is too short";
-        ifSendData = false;
-    }
+  if (passwordValue.length === 0) {
+    passwordMsg.innerText = "Please enter your password";
+    ifSendData = false;
+  } else if (passwordValue.length <= 4) {
+    passwordMsg.innerText = "Your password is too short";
+    ifSendData = false;
+  }
 
-    if (ifSendData) {
-        const body = JSON.stringify({
-            username: usernameValue,
-            password: passwordValue,
-        })
-        const headers = {
-            "Content-Type": "application/json"
-        }
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: "POST",
-            body: body,
-            headers: headers
-        })
-            .then(response => {
-                if(response.ok) {
-                    sigininMsg.innerText = "You signed in successfully"
-                }
-            })
-    }
+  if (ifSendData) {
+    const body = JSON.stringify({
+      username: usernameValue,
+      password: passwordValue,
+    });
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: body,
+      headers: headers,
+    }).then((response) => {
+      if (response.ok) {
+        sigininMsg.innerText = "You signed in successfully";
+      }
+    });
+  }
 }
